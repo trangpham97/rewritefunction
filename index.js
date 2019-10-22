4c504f0b7f46bdfc61b75b8616e1108966d0f214
trangpham97 <trangpn9a4@gmail.com>
2019-10-22 08:44:58 +0700

    clean

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ function map(arr, handleFunction) {
   const length = arr.length;
   for (let i = 0; i < length; i++) {
     const result = handleFunction(arr[i]);
-    newArr.push(result);    
+    newArr.push(result);
   }
   return newArr;
 };
 
-const arr = [1, 2, 3];
-console.log(map(arr, (x) => x * 2));
 
 
 //--- FILTER -----
@@ -26,16 +24,6 @@ function filter(arr, handleFunction) {
   return newArr;
 };
 
-const ages = [32, 33, 16, 40];
-
-function checkAdult(age) {
-  return age >= 18;
-}
-
-console.log(filter(ages, checkAdult));
-
-const arrFilter = [1, 2, 3, 4, 5, 6, 7, 8];
-console.log(filter(arrFilter, num => num % 2 === 0));
 
 
 //--- FIND -----
@@ -52,29 +40,15 @@ function find(arr, handleFunction) {
   return newArr;
 };
 
-const agesFind = [32, 33, 16, 40];
-
-function checkAdult(age) {
-  return age >= 18;
-}
-
-console.log(find(agesFind, checkAdult));
 
 
 //--- CLONE DEEP -----
-
-
 const testObject = {
   a: 1,
   b: 2,
   c: 3
 };
 const cloned = JSON.parse(JSON.stringify(testObject));
-
-console.log(cloned);
-cloned.a = 2;
-console.log(cloned);
-console.log(testObject);
 
 
 
@@ -83,6 +57,3 @@ function concat(arr1, arr2) {
   return arr1.join(',') + ',' + arr2.join(',');
 }
 
-const hege = ["Cecilie", "Lone"];
-const stale = ["Emil", "Tobias", "Linus"];
-console.log(concat(hege, stale));
