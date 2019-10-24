e4d3fb9ed5084eea7559cb93e064282e998f928c
trangpham97 <trangpn9a4@gmail.com>
2019-10-24 09:55:36 +0700

    curried func

--- index.js
+++ index.js
@@ -1,46 +1,31 @@
 //--- MAP -----
-function map(arr, handleFunction) {
+const map = arr => func => {
   const newArr = [];
-  const length = arr.length;
-  for (let i = 0; i < length; i++) {
-    const result = handleFunction(arr[i]);
-    newArr.push(result);
-  }
+  arr.forEach(element => {
+    newArr.push(func(element));
+  });
   return newArr;
-};
-
-
+}
 
 //--- FILTER -----
-function filter(arr, handleFunction) {
+const filter = arr => func => {
   const newArr = [];
-  const length = arr.length;
-  for (let i = 0; i < length; i++) {
-    if (handleFunction(arr[i]) === true) {
-      const result = arr[i];
-      newArr.push(result);
+  arr.forEach(element => {
+    if (func(element) === true) {
+      newArr.push(element);
     }
-  }
+  });
   return newArr;
-};
-
-
+}
 
 //--- FIND -----
-function find(arr, handleFunction) {
-  const newArr = [];
-  const length = arr.length;
-  for (let i = 0; i < length; i++) {
-    if (handleFunction(arr[i]) === true) {
-      const result = arr[i];
-      newArr.push(result);
-      break;
+const find = arr => func => {
+  for(const element of arr){
+    if (func(element) === true) {   
+      return element;    
     }
   }
-  return newArr;
-};
-
-
+}
 
 //--- CLONE DEEP -----
 const testObject = {
@@ -50,10 +35,7 @@ const testObject = {
 };
 const cloned = JSON.parse(JSON.stringify(testObject));
 
-
-
 //--- CONCAT -----
-function concat(arr1, arr2) {
+const concat = arr1 => arr2 => {
   return arr1.join(',') + ',' + arr2.join(',');
 }
-
